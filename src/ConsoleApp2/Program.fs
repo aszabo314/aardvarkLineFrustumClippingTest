7d519d371a6a78cbf138448d96709df7ad884684
Attila Szabo <aszabo@vrvis.at>
2021-03-23 13:05:45 +0100

    clipLine?

--- src/ConsoleApp2/Program.fs
+++ src/ConsoleApp2/Program.fs
@@ -10,13 +10,44 @@ open System.Runtime.InteropServices
 
 #nowarn "9"
 
-let intersectPlaneLine (p : Plane3d) (l : Line3d) =
-    let ray = Ray3d(l.P0, (l.P1-l.P0).Normalized)
-    let t0 = 0.0
-    let t1 = ray.GetTOfProjectedPoint(l.P1)
-    let hit = ray.Intersect p
-    let tHit = ray.GetTOfProjectedPoint(hit)
-    if tHit >= t0 && tHit <= t1 then Some (tHit, ray.GetPointOnRay tHit)
+let clipLine (line : Line3d) (hull : Hull3d) =
+    let planes = hull.PlaneArray
+    let mutable p0 = line.P0
+    let mutable p1 = line.P1
+
+    let rec contains (pi : int) (i : int) (pt : V3d) =
+        if i >= planes.Length then
+            true
+        elif pi = i then
+            contains pi (i+1) pt
+        else
+            planes.[i].Height pt <= 0.0 && contains pi (i+1) pt
+
+    let mutable valid = true
+    let mutable pi = 0
+    while pi < planes.Length do
+        let p = planes.[pi]
+        let h0 = p.Height p0
+        let h1 = p.Height p1
+
+        if h0 < 0.0 && h1 > 0.0 then
+            // p0 inside
+            let pt = (p0 * (h1 + p.Distance) - p1 * (h0 + p.Distance)) / (h1 - h0)
+            if contains pi 0 pt then
+                p1 <- pt
+        elif h1 < 0.0 && h0 > 0.0 then
+            // p1 inside
+            let pt = (p0 * (h1 + p.Distance) - p1 * (h0 + p.Distance)) / (h1 - h0)
+            if contains pi 0 pt then
+                p0 <- pt
+        elif h0 > 0.0 && h1 > 0.0 then
+            // both outside
+            valid <- false
+            pi <- planes.Length
+
+        pi <- pi + 1
+
+    if valid then Line3d(p0, p1) |> Some
     else None
 
 [<EntryPoint>]
@@ -48,35 +79,23 @@ let main argv =
 
     let line = AVal.init (Line3d(V3d.OOO, V3d.OOI * 8.0))
 
+    let frustumViewTrafo =
+        adaptive {
+            let! d = debugEnabled
+            if d = 0 then 
+                return! viewTrafo
+            else 
+                return! normalView
+        }
     let clippedLine =
-        AVal.bind (fun p -> 
-            AVal.map2 (fun v (l : Line3d) -> 
-                let vp = CameraView.viewTrafo v * Frustum.projTrafo p
-                let hull = ViewProjection.toHull3d vp
-
-                let mutable ts = []
-                for (p : Plane3d) in hull.PlaneArray do
-                    match intersectPlaneLine p l with 
-                    | Some (t,hit) ->
-                        if hull.Contains(hit) then 
-                            ts <- (t,hit)::ts
-                    | None -> ()
-
-                match ts with 
-                | [] -> if hull.Contains l.P0 || hull.Contains l.P1 then Some l else Log.warn "empty"; None 
-                | [t,hit] -> 
-                    if hull.Contains l.P0 then Some (Line3d(l.P0,hit))
-                    elif hull.Contains l.P1 then Some (Line3d(hit,l.P1))
-                    else Log.error "very bad"; None
-                | [(t0,hit0);(t1,hit1)] -> 
-                    if t0 <= t1 then Some (Line3d(hit0,hit1))
-                    else Some (Line3d(hit1,hit0))
-                | _ -> Log.error "wtf %d" ts.Length; None
-
-            ) normalView line
-        ) normalProj
+        AVal.map3 (fun v (l : Line3d) p -> 
+            //let vp = CameraView.viewTrafo v * Frustum.projTrafo p
+            //let hull = ViewProjection.toHull3d vp
+            let b = Box3d.FromCenterAndSize (V3d.OOI * 4.0, V3d.III)
+            let hull = Hull3d.Create b
+            clipLine l hull
+        ) frustumViewTrafo line normalProj
         
-
     win.Keyboard.Down.Values.Add (fun k -> 
         match k with 
         | Keys.Space -> 
@@ -90,15 +109,6 @@ let main argv =
             )
         | _ -> ()
     ) 
-
-    let frustumViewTrafo =
-        adaptive {
-            let! d = debugEnabled
-            if d = 0 then 
-                return! viewTrafo
-            else 
-                return! normalView
-        }
 
     let frustumSg = 
         Sg.wireBox' C4b.DeepPink (Box3d(V3d.NNN, V3d.III))
@@ -123,13 +133,22 @@ let main argv =
 
     let clippedLineSg =
         clippedLine |> AVal.map (fun l -> 
-            Sg.lines (AVal.constant C4b.LightGoldenRodYellow) (AVal.constant (l |> Option.toArray))
-            |> Sg.shader {
-                do! DefaultSurfaces.trafo
-                do! DefaultSurfaces.thickLine
-                do! DefaultSurfaces.vertexColor
-            }
-            |> Sg.uniform "LineWidth" (AVal.constant 3.5)
+            match l with 
+            | Some l ->
+                let box0 =
+                    Sg.wireBox' C4b.LightGoldenRodYellow (Box3d.FromCenterAndSize(l.P0,V3d.III*0.1))
+                let box1 =
+                    Sg.wireBox' C4b.LightGoldenRodYellow (Box3d.FromCenterAndSize(l.P1,V3d.III*0.1))
+                let line = 
+                    Sg.lines (AVal.constant C4b.LightGoldenRodYellow) (AVal.constant [|l|])
+                Sg.ofList [box0;box1;line]
+                |> Sg.shader {
+                    do! DefaultSurfaces.trafo
+                    do! DefaultSurfaces.thickLine
+                    do! DefaultSurfaces.vertexColor
+                }
+                |> Sg.uniform "LineWidth" (AVal.constant 3.5)
+            | None -> Sg.empty
         ) |> Sg.dynamic
     
     let sg =
